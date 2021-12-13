c248e759545f39f3a74d85e583858a5883299089
shalinishal4444 <shalinishal4444@gmail.com>
2021-12-13 16:10:16 +0530

    my task done

---
+++ src/org/test/Sample1.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class Sample1 {
+	public static void main(String[] args) {
+		int num =36;
+		if(num%2 == 36) {
+			System.out.println("even");
+		}else {
+			System.out.println("odd");
+
+}
+}}
